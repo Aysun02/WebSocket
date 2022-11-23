54f199ba65d9bff137e4310fce49edde67a86e4d
Aysun02 <aysunvh@code.edu.az>
2022-11-23 12:37:40 +0400

    add

---
+++ SignalR/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SignalR.Models;
+using System.Diagnostics;
+
+namespace SignalR.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
